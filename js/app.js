2f38f79d8f69848c627c1294070650b6b7a246ef
NDITAKUNDA <126288432+NDITAKUNDA@users.noreply.github.com>
2024-05-19 14:27:32 +0200

    Javascript version updated

--- js/app.js
+++ js/app.js
@@ -96,41 +96,40 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   document.querySelector('.main-couresel-container').appendChild(carouselTrack);
-});
 
+  // Code for the couresel
+  const track = document.querySelector(".carousel-track");
+  const slides = Array.from(track.children);
+  const slideWidth = slides[0].getBoundingClientRect().width;
 
-// Code for the couresel
-const track = document.querySelector(".carousel-track");
-const slides = Array.from(track.children);
-const slideWidth = slides[0].getBoundingClientRect().width;
+  // Arrange slides horizontally
+  const setSlidePosition = (slide, index) => {
+    slide.style.left = slideWidth * index + "px";
+  };
+  slides.forEach(setSlidePosition);
 
-// Arrange slides horizontally
-const setSlidePosition = (slide, index) => {
-  slide.style.left = slideWidth * index + "px";
-};
-slides.forEach(setSlidePosition);
+  // Next and previous buttons
+  const nextButton = document.getElementById("next-btn");
+  nextButton.textContent = ">";
+  nextButton.addEventListener("click", () => {
+    const currentSlide = track.querySelector(".current-slide");
+    const nextSlide = currentSlide.nextElementSibling;
+    if (!nextSlide) {
+      nextSlide = slides[slides.length - 1];
+    }
+    currentSlide.classList.remove("current-slide");
+    nextSlide.classList.add("current-slide");
+  });
 
-// Next and previous buttons
-const nextButton = document.getElementById("next-btn");
-nextButton.textContent = ">";
-nextButton.addEventListener("click", () => {
-  const currentSlide = track.querySelector(".current-slide");
-  const nextSlide = currentSlide.nextElementSibling;
-  if (!nextSlide) {
-    nextSlide = slides[slides.length - 1];
-  }
-  currentSlide.classList.remove("current-slide");
-  nextSlide.classList.add("current-slide");
-});
-
-const prevButton = document.getElementById("prev-btn");
-prevButton.textContent = "<";
-prevButton.addEventListener("click", () => {
-  const currentSlide = track.querySelector(".current-slide");
-  const prevSlide = currentSlide.previousElementSibling;
-  if (!prevSlide) {
-    prevSlide = slides[1]
-  }
-  currentSlide.classList.remove("current-slide");
-  prevSlide.classList.add("current-slide");
+  const prevButton = document.getElementById("prev-btn");
+  prevButton.textContent = "<";
+  prevButton.addEventListener("click", () => {
+    const currentSlide = track.querySelector(".current-slide");
+    const prevSlide = currentSlide.previousElementSibling;
+    if (!prevSlide) {
+      prevSlide = slides[1]
+    }
+    currentSlide.classList.remove("current-slide");
+    prevSlide.classList.add("current-slide");
+  });
 });
